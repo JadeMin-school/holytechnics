f5215d9070e11bb803c0e49858ee9a33ff463fa6
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-05-17 00:31:43 +0900

    refactor: clean indentation

--- board/write/index.php
+++ board/write/index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-<>
-	<?php require_once("../../lib/db.php"); ?>
-	<?php ?>
+	<head>
+		<?php require_once("../../lib/db.php"); ?>
+		<?php ?>
 
-	<meta charset="UTF-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>게시글 작성</title>
-</head>
-<body>
-	
-</body>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>게시글 작성</title>
+	</head>
+	<body>
+		
+	</body>
 </html>
