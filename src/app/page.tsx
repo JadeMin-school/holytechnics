5a9185229968d8847e79910f124365e006007bcc
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-06-02 15:15:12 +0900

    upload: next.js deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,13 @@ export default async function Home(): Promise<JSX.Element> {
 				</thead>
 				<tbody>
 					{
-						data.map((post) => {
-							return (
-								<tr key={post.id}>
-									<td>{post.id}</td>
-									<td>{post.title}</td>
-									<td>{post.body}</td>
-								</tr>
-							)
-						})
+						data.map((post) => (
+							<tr key={post.id}>
+								<td>{post.id}</td>
+								<td>{post.title}</td>
+								<td>{post.body}</td>
+							</tr>
+						))
 					}
 				</tbody>
 			</table>
