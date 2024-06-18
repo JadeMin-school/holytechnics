0fc004861471624e55acb5c6e39c69bd5f794e40
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-06-18 20:05:01 +0900

    update

--- src/app/components/navbar/randomKeyword.ts
+++ src/app/components/navbar/randomKeyword.ts
@@ -1,8 +1,8 @@
 const keywords = [
 	"오늘의 학식",
 	"오늘의 시간표",
-] as const;
+];
 
-export function randomKeyword() {
+export function randomKeyword(): string {
 	return keywords[Math.floor(Math.random() * keywords.length)];
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,8 @@
 import Navbar from "@/app/components/navbar";
 import Footer from "@/app/components/footer";
 
+import "./page.css";
+
 
 
 export default function Login() {
@@ -10,7 +12,22 @@ export default function Login() {
 		<>
 			<Navbar/>
 			<main>
-				<h1>로그인 페이지</h1>
+				<h1>로그인</h1>
+				<form>
+					<input
+						type="text"
+						placeholder="아이디"
+					/>
+					<input
+						type="password"
+						placeholder="비밀번호"
+					/>
+					<button type="submit">로그인</button>
+				</form>
+				<hr/>
+				<p>
+					아직 회원이 아니신가요? <a href="/register">회원가입</a>
+				</p>
 			</main>
 			<Footer/>
 		</>
